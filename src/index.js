09e8956b5f5ab0baf0996cc51b4717e1b961a76c
Abel van Hoek <abelvanhoek@gmail.com>
2021-05-31 11:50:49 +0200

    fix error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import useAsyncEffect from './use_async_effect'
 import useComponentWillMount from './use_component_will_mount'
 import useConcatState from './use_concat_state'
-import useIntervalState from './use_interval_state'
+import useInterval from './use_interval'
 import useMergeState from './use_merge_state'
 import usePrevious from './use_previous'
 import useScrollToTop from './use_scroll_to_top'
 import useToggleState from './use_toggle_state'
 
-export {useAsyncEffect, useConcatState, useComponentWillMount, useIntervalState, useMergeState, usePrevious, useScrollToTop, useToggleState}
+export {useAsyncEffect, useConcatState, useComponentWillMount, useInterval, useMergeState, usePrevious, useScrollToTop, useToggleState}
