c6c1b06e5e9a8104dd6e4f5e49cb23f0a3b4c813
Christiaan <ckreeftmeijer@gmail.com>
2024-01-08 13:30:47 +0100

    add hook to export

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import useAsyncEffect from './use_async_effect'
-import useComponentWillMount from './use_component_will_mount'
-import useConcatState from './use_concat_state'
-import useInterval from './use_interval'
-import useMergeState from './use_merge_state'
-import usePrevious from './use_previous'
-import useScrollToTop from './use_scroll_to_top'
-import useToggleState from './use_toggle_state'
-import useBoolState from './use_bool_state'
-import useIncrementState from './use_increment_state'
-import useCountup from './use_countup'
-import useCountdown from './use_countdown'
+import useAsyncEffect from "./use_async_effect";
+import useComponentWillMount from "./use_component_will_mount";
+import useConcatState from "./use_concat_state";
+import useInterval from "./use_interval";
+import useMergeState from "./use_merge_state";
+import usePrevious from "./use_previous";
+import useScrollToTop from "./use_scroll_to_top";
+import useToggleState from "./use_toggle_state";
+import useBoolState from "./use_bool_state";
+import useIncrementState from "./use_increment_state";
+import useCountup from "./use_countup";
+import useCountdown from "./use_countdown";
+export * from "./use_hover";
 
 export {
   useAsyncEffect,
@@ -24,4 +25,4 @@ export {
   useIncrementState,
   useCountup,
   useCountdown,
-}
+};
